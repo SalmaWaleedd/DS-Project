88c5ff4f5b772abc7a56b5764fc5abe2635f6bea
MohamedFareed1 <133539866+MohamedFareed1@users.noreply.github.com>
2024-01-07 22:30:36 +0200

    Add files via upload

---
+++ h/minifying.h
@@ -0,0 +1,15 @@
+
+/*******************************************************************
+* File Name: minifying.h
+* Description: Header file that minifying xml file
+********************************************************************/
+#pragma once
+#ifndef MINIFYING_H
+#define MINIFYING_H
+
+#include <string>
+using namespace std;
+
+string minifying(const string input_text);
+
+#endif // MINIFYING_H
