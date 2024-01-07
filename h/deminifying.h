3e39782d9953238a43daf643d009d2d25e085844
MohamedFareed1 <133539866+MohamedFareed1@users.noreply.github.com>
2024-01-07 22:29:39 +0200

    Add files via upload

---
+++ h/deminifying.h
@@ -0,0 +1,14 @@
+
+/*******************************************************************
+* File Name: deminifying.h
+* Description: Header file that deminifying xml file
+********************************************************************/
+#pragma once
+#ifndef DEMINIFYING_H
+#define DEMINIFYING_H
+
+#include <string>
+using namespace std;
+string deminifying(const string input);
+
+#endif // DEMINIFYING_H
