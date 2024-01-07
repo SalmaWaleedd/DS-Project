bcafc1a8cb7b2358cddf7cb8ede7e576323a808a
Shadi mohamed <132169584+shadimohamed1@users.noreply.github.com>
2024-01-07 22:11:27 +0200

    Add files via upload

---
+++ h/prettifying.h
@@ -0,0 +1,18 @@
+#pragma once
+/*******************************************************************
+* File Name: prettifying.h
+* Description: Header file that prettify xml file
+********************************************************************/
+#pragma once
+#ifndef PRETTIFYING_H
+#define PRETTIFYING_H
+
+
+#include <string>
+using namespace std;
+
+string prettifying(const string input);
+
+
+
+#endif
